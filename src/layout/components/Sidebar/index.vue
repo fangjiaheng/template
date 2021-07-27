62666744e0f7ac110bd31a05ff541142d055bef3
unknown <631657480@qq.com>
2021-07-27 21:45:38 +0800

    scrollbar差不多会用

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
+    <logo />
     aliadsas
   </div>
 </template>
 
 <script>
+import Logo from './components/Logo.vue'
 export default {
-
+  components: {
+    Logo
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    登录123
+  <div class="test">
+    <el-scrollbar class="box">
+      <div class="child">
+        123
+      </div>
+    </el-scrollbar>
   </div>
 </template>
 
@@ -10,5 +14,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .test{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .box{
+      width: 200px;
+      height: 300px;
+      background-color: #ccc;
+      // overflow-x: auto;
+      // overflow-y: auto;
+      .child{
+        width: 100%;
+        height: 600px;
+        background-color: aqua;
+      }
+    }
+  }
 </style>
+
+// <style lang="scss">
+//   .el-scrollbar .el-scrollbar__wrap{
+//     overflow-x: hidden;
+//   }
+//   .is-horizontal{
+//     display: none;
+//   }
+// </style>
